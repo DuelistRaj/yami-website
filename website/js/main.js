4bb229941ac2d8b20e60628bc4b4836dbd9dfc96
duelistraj <duelistrahul@gmail.com>
2019-09-29 23:51:13 +0530

    new design

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,5 @@
+let root = document.documentElement;
+
 jQuery(document).ready(function () {
     // Transition effect for navbar 
     $(window).scroll(function () {
@@ -9,3 +11,21 @@ jQuery(document).ready(function () {
         }
     });
 });
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("gallery-slide");
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    
+    if(slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 50000);
+}
