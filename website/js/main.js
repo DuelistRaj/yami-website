37ec1ec8a3ce88c9b4d8737863d606b2c904bb7f
duelistraj <duelistrahul@gmail.com>
2019-10-01 01:25:07 +0530

    completed navbar + optimization for mobile

--- website/js/main.js
+++ website/js/main.js
@@ -6,11 +6,31 @@ jQuery(document).ready(function () {
         // checks if window is scrolled more than 500px, adds/removes solid class
         if ($(this).scrollTop() > 80) {
             $("header").addClass('solid');
+            $("#mobile-nav").addClass('solid');
         } else {
             $("header").removeClass('solid');
+            $("#mobile-nav").removeClass('solid');
         }
     });
 });
+
+jQuery(document).ready(function () {
+    $("#m-nav-icon").click(function () {
+        if ($("#mobile-nav").hasClass('mobile-hidden')) {
+            $("#mobile-nav").removeClass('mobile-hidden');
+        }
+        else {
+            $("#mobile-nav").addClass('mobile-hidden');
+        }
+
+    });
+});
+
+$(window).bind("resize", function () {
+    if ($(this).width() > 720) {
+        $('#mobile-nav').addClass('mobile-hidden');
+    }
+}).trigger('resize');
 
 var slideIndex = 0;
 showSlides();
@@ -18,12 +38,12 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("gallery-slide");
-    for(i = 0; i < slides.length; i++) {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    
-    if(slideIndex > slides.length) {
+
+    if (slideIndex > slides.length) {
         slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
