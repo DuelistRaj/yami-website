c71ce702e8416a8a5fdd6e02f0c2e41004ccbe06
duelistraj <duelistrahul@gmail.com>
2019-10-07 03:29:29 +0530

    Feedback Update 1.1

---
+++ website/js/stickyfooter.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var element = document.body;
+    var height = element.offsetHeight;
+    if (height < screen.height) {
+        $("#footer").addClass('stickybottom');
+    }
+    else {
+        $("#footer").removeClass('stickybottom');
+    }
+}, false);
